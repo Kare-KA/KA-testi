1ddcee83850ca8dcc0772b5552d84ad412e23d81
Kare-KA <kare.salonvaara@kansallisarkisto.fi>
2025-04-23 14:56:05 +0300

    Create klaro.js

---
+++ klaro.js
@@ -0,0 +1,89 @@
+{
+  "version": 1,
+  "cookieExpiresAfterDays": 60,
+  "name": "default",
+  "status": "active",
+  "id": "07eff4f3097c0e3f7e9d8090b3129a92",
+  "elementID": "klaro",
+  "storageMethod": "cookie",
+  "cookieName": "klaro",
+  "lang": "zz",
+  "languages": [
+    "en"
+  ],
+  "hideToggleAll": false,
+  "htmlTexts": false,
+  "embedded": false,
+  "groupByPurpose": true,
+  "purposeOrder": [
+    "analytics"
+  ],
+  "default": false,
+  "mustConsent": false,
+  "disablePoweredBy": false,
+  "acceptAll": true,
+  "hideDeclineAll": false,
+  "noticeAsModal": false,
+  "translations": {
+    "en": {
+      "purposes": {
+        "Analytics": {},
+        "analytics": {}
+      }
+    },
+    "zz": {
+      "purposes": {
+        "Analytics": {},
+        "analytics": {}
+      }
+    }
+  },
+  "styling": {
+    "theme": []
+  },
+  "services": [
+    {
+      "id": "",
+      "version": 1,
+      "name": "matomo",
+      "purposes": [
+        "analytics"
+      ],
+      "default": false,
+      "required": false,
+      "optOut": false,
+      "contextualConsentOnly": false,
+      "onlyOnce": false,
+      "translations": {
+        "en": {},
+        "zz": {}
+      },
+      "cookies": [
+        {
+          "pattern": "/^pk.*$/",
+          "expiresAfter": "",
+          "path": "",
+          "_id": "_0kjtrkuaz"
+        },
+        {
+          "pattern": "/^mtm.*$/",
+          "expiresAfter": "",
+          "path": "",
+          "_id": "_7thymq9ie"
+        },
+        {
+          "pattern": "/^MATOMO.*$/",
+          "expiresAfter": "",
+          "path": "",
+          "_id": "_legdxhpl2"
+        }
+      ],
+      "localStorage": [],
+      "sessionStorage": [],
+      "elements": null,
+      "requests": [],
+      "actions": null,
+      "_id": "_zeeoon791"
+    }
+  ]
+}
