29d255eb08af4caf99d74935b4d3f6e5d85d10c6
Kare-KA <kare.salonvaara@kansallisarkisto.fi>
2025-05-03 02:07:06 +0300

    klaro edit

---
+++ klaro/backup/config.js
@@ -0,0 +1,28 @@
+
+var klaroConfig = {
+    version: 1,
+    elementID: 'klaro',
+    styling: {
+        theme: ['none']
+    },
+    noticeAsModal: false,
+    mustConsent: false,
+    noticeOnly: true,
+    acceptAll: true,
+    hideDeclineAll: false,
+    hideLearnMore: true,
+    htmlTexts: true,
+    embedded: false,
+    showNoticeTitle: false,
+    lang: 'fi',
+    translations: {
+        fi: {
+            consentNotice: {
+                description: 'Käytämme teknologioita, kuten evästeitä, tarjotaksemme paremman käyttökokemuksen.'
+            },
+            acceptAll: 'Kyllä',
+            decline: 'Ei'
+        }
+    },
+    services: []
+};
