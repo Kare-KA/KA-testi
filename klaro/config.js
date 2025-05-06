33857b96222471335c462b5b0ac698f786c8f66a
Kare-KA <kare.salonvaara@kansallisarkisto.fi>
2025-05-06 16:59:32 +0300

    Update config.js

--- klaro/config.js
+++ klaro/config.js
@@ -128,7 +128,7 @@ var klaroConfig = {
     fi: {
         privacyPolicyUrl: '/#privacy',
         consentNotice: {
-            description: 'Sivustomme käyttää evästeitä parantaakseen käyttökokemusta. <a href="/privacy">Lue lisää</a>'
+            description: 'Sivustomme käyttää evästeitä parantaakseen käyttökokemusta. <a href="#" data-klaro="open-modal">Lue lisää</a>'
         },
         consentModal: {
             description: 'Tässä voit valita, mihin evästeisiin suostut.'
